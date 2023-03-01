b06b7239c53d1199aa1421dd15f15d874ee059cd
Cecilie Fredsgaard <ceciliefredsgaard96@gmail.com>
2023-03-01 20:15:34 +0100

    Spelling mistake fixed in Player

--- AsteroidsServiceLoader/Player/src/main/java/dk/sdu/mmmi/cbse/playersystem/PlayerPlugin.java
+++ AsteroidsServiceLoader/Player/src/main/java/dk/sdu/mmmi/cbse/playersystem/PlayerPlugin.java
@@ -24,7 +24,7 @@ public class PlayerPlugin implements IGamePluginService {
 
     private Entity createPlayerShip(GameData gameData) {
 
-        float deacceleration = 10;
+        float deceleration = 10;
         float acceleration = 200;
         float maxSpeed = 300;
         float rotationSpeed = 5;
@@ -33,8 +33,8 @@ public class PlayerPlugin implements IGamePluginService {
         float radians = 3.1415f / 2;
 
         Entity playerShip = new Player();
-        playerShip.setRadius(8);
-        playerShip.add(new MovingPart(deacceleration, acceleration, maxSpeed, rotationSpeed));
+        playerShip.setRadius(10);
+        playerShip.add(new MovingPart(deceleration, acceleration, maxSpeed, rotationSpeed));
         playerShip.add(new PositionPart(x, y, radians));
         playerShip.add(new LifePart(1));
 
