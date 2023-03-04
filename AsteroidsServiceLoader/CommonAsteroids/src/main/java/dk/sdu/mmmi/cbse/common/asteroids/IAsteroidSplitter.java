af38aaf9e77fec5c056d77de4b685612c78409de
Cecilie Fredsgaard <ceciliefredsgaard96@gmail.com>
2023-03-04 16:01:41 +0100

    Update IAsteroidSplitter interface
    Added GameData as parameter for the method

--- AsteroidsServiceLoader/CommonAsteroids/src/main/java/dk/sdu/mmmi/cbse/common/asteroids/IAsteroidSplitter.java
+++ AsteroidsServiceLoader/CommonAsteroids/src/main/java/dk/sdu/mmmi/cbse/common/asteroids/IAsteroidSplitter.java
@@ -1,12 +1,15 @@
 package dk.sdu.mmmi.cbse.common.asteroids;
 
 import dk.sdu.mmmi.cbse.common.data.Entity;
+import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
 
 /**
  *
  * @author corfixen
+ *
+ * This interface is not being used
  */
 public interface IAsteroidSplitter {
-    void createSplitAsteroid(Entity e, World w);
+    void createSplitAsteroid(Entity entity, GameData gameData, World world);
 }
