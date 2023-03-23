1346c3cdfe8c480f1eed798f1b1866131cac4499
Cecilie Fredsgaard <ceciliefredsgaard96@gmail.com>
2023-03-23 18:21:25 +0100

    Create SizePart (new Entity part)
    To be used by Asteroid Implementation

---
+++ AsteroidsServiceLoader/Common/src/main/java/dk/sdu/mmmi/cbse/common/data/entityparts/SizePart.java
@@ -0,0 +1,25 @@
+package dk.sdu.mmmi.cbse.common.data.entityparts;
+
+import dk.sdu.mmmi.cbse.common.data.Entity;
+import dk.sdu.mmmi.cbse.common.data.GameData;
+
+public class SizePart implements EntityPart{
+
+    private String size;
+
+    public SizePart(String size) {
+        this.size = size;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    @Override
+    public void process(GameData gameData, Entity entity) {
+    }
+}
