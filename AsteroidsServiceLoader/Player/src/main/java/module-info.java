3f81926313c749298dbbdbc3811af8949cbd10ac
Cecilie Fredsgaard <ceciliefredsgaard96@gmail.com>
2023-03-23 16:17:19 +0100

    Player module-info provides and uses added
    Provides made sure player was in the game and being processed
    
    Uses made it possible to shoot

--- AsteroidsServiceLoader/Player/src/main/java/module-info.java
+++ AsteroidsServiceLoader/Player/src/main/java/module-info.java
@@ -1,6 +1,13 @@
+import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
+import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
+
 module Player {
     requires Common;
     requires CommonBullet;
     requires CommonPlayer;
 
+    provides IGamePluginService with dk.sdu.mmmi.cbse.playersystem.PlayerPlugin;
+    provides IEntityProcessingService with dk.sdu.mmmi.cbse.playersystem.PlayerControlSystem;
+
+    uses dk.sdu.mmmi.cbse.common.bullet.BulletSPI;
 }
