2772a6ad800e703387c75f4f72cbcc28f48cd392
Cecilie Fredsgaard <ceciliefredsgaard96@gmail.com>
2023-03-23 18:24:36 +0100

    Create Asteroids with SizePart and use Entity as type instead of Asteroid
    SizePart is set to the values in Enum, but that is specific to this implementation of Asteroids.

--- AsteroidsServiceLoader/Asteroid/src/main/java/dk/sdu/mmmi/cbse/asteroidsystem/AsteroidFactory.java
+++ AsteroidsServiceLoader/Asteroid/src/main/java/dk/sdu/mmmi/cbse/asteroidsystem/AsteroidFactory.java
@@ -1,10 +1,12 @@
 package dk.sdu.mmmi.cbse.asteroidsystem;
 
+import dk.sdu.mmmi.cbse.common.data.Entity;
 import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.entityparts.LifePart;
 import dk.sdu.mmmi.cbse.common.data.entityparts.MovingPart;
 import dk.sdu.mmmi.cbse.common.data.entityparts.PositionPart;
-
+import dk.sdu.mmmi.cbse.common.data.entityparts.SizePart;
+import dk.sdu.mmmi.cbse.common.asteroids.Asteroid;
 
 public class AsteroidFactory {
     // Moving part data
@@ -14,6 +16,9 @@ public class AsteroidFactory {
     int life = 0;
     float expiration = 0;
 
+    // Size part data
+    String size = "";
+
     AsteroidFactory(){}
 
     /***
@@ -21,9 +26,9 @@ public class AsteroidFactory {
      * @param asteroid an asteroid with a size, but no moving or life part values.
      * @return asteroid with correct moving and life part values associated with the size of the asteroid
      */
-    private Asteroid createAsteroidWithLifeAndMovementParts(Asteroid asteroid) {
+    private Entity createAsteroidWithLifeAndMovementParts(Entity asteroid, AsteroidSize asteroidSize) {
 
-        switch (asteroid.getAsteroidSize()) {
+        switch (asteroidSize) {
             case GIANT_SIZE -> {
                 // MovingPart Data
                 deceleration = 0;
@@ -34,6 +39,9 @@ public class AsteroidFactory {
                 // LifePart Data
                 life = AsteroidLife.GIANT_LIFE.getLife();
                 expiration = 15.3f;
+
+                //SizePart Data
+                size = AsteroidSize.GIANT_SIZE.getAsteroidSize();
 
                 asteroid.setRadius(30);
             }
@@ -47,6 +55,10 @@ public class AsteroidFactory {
                 // LifePart Data
                 life = AsteroidLife.LARGE_LIFE.getLife();
                 expiration = 15.3f;
+
+                //SizePart Data
+                size = AsteroidSize.LARGE_SIZE.getAsteroidSize();
+
                 asteroid.setRadius(25);
             }
             case MEDIUM_SIZE -> {
@@ -59,6 +71,10 @@ public class AsteroidFactory {
                 // LifePart Data
                 life = AsteroidLife.MEDIUM_LIFE.getLife();
                 expiration = 15.3f;
+
+                //SizePart Data
+                size = AsteroidSize.MEDIUM_SIZE.getAsteroidSize();
+
                 asteroid.setRadius(15);
             }
             case SMALL_SIZE -> {
@@ -71,6 +87,10 @@ public class AsteroidFactory {
                 // LifePart Data
                 life = AsteroidLife.SMALL_LIFE.getLife();
                 expiration = 15.3f;
+
+                //SizePart Data
+                size = AsteroidSize.SMALL_SIZE.getAsteroidSize();
+
                 asteroid.setRadius(8);
             }
         }
@@ -78,6 +98,7 @@ public class AsteroidFactory {
         // Add the parts to the asteroid
         asteroid.add(new MovingPart(deceleration, acceleration, maxSpeed, rotationSpeed));
         asteroid.add(new LifePart(life));
+        asteroid.add(new SizePart(size));
 
         // Returns the asteroid with all the parts
         return asteroid;
@@ -93,29 +114,29 @@ public class AsteroidFactory {
     }
     
     // Create a giant asteroid
-    public Asteroid createGiantAsteroid(GameData gameData) {
-        Asteroid asteroid = createAsteroidWithLifeAndMovementParts(new Asteroid(AsteroidSize.GIANT_SIZE));
+    public Entity createGiantAsteroid(GameData gameData) {
+        Entity asteroid = createAsteroidWithLifeAndMovementParts(new Asteroid(), AsteroidSize.GIANT_SIZE);
         asteroid.add(randomPosition(gameData));
         return asteroid;
     }
 
     // Create a large asteroid
-    public Asteroid createLargeAsteroid(GameData gameData) {
-        Asteroid asteroid =  createAsteroidWithLifeAndMovementParts(new Asteroid(AsteroidSize.LARGE_SIZE));
+    public Entity createLargeAsteroid(GameData gameData) {
+        Entity asteroid =  createAsteroidWithLifeAndMovementParts(new Asteroid(), AsteroidSize.LARGE_SIZE);
         asteroid.add(randomPosition(gameData));
         return asteroid;
     }
 
     // Create a medium asteroid
-    public Asteroid createMediumAsteroid(GameData gameData) {
-        Asteroid asteroid = createAsteroidWithLifeAndMovementParts(new Asteroid(AsteroidSize.MEDIUM_SIZE));
+    public Entity createMediumAsteroid(GameData gameData) {
+        Entity asteroid = createAsteroidWithLifeAndMovementParts(new Asteroid(), AsteroidSize.MEDIUM_SIZE);
         asteroid.add(randomPosition(gameData));
         return asteroid;
     }
 
     // Create a small asteroid
-    public Asteroid createSmallAsteroid(GameData gameData) {
-        Asteroid asteroid = createAsteroidWithLifeAndMovementParts(new Asteroid(AsteroidSize.SMALL_SIZE));
+    public Entity createSmallAsteroid(GameData gameData) {
+        Entity asteroid = createAsteroidWithLifeAndMovementParts(new Asteroid(), AsteroidSize.SMALL_SIZE);
         asteroid.add(randomPosition(gameData));
         return asteroid;
     }
