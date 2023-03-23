eaa3c7a178725160dfc0843b69dd15635d466f25
Cecilie Fredsgaard <ceciliefredsgaard96@gmail.com>
2023-03-23 16:19:20 +0100

    EnemyShip module-info provides and uses added
    Provides made sure enemyship is in the game and being processed
    
    Uses made it possible for the enemyship to shoot

--- AsteroidsServiceLoader/EnemyShip/src/main/java/module-info.java
+++ AsteroidsServiceLoader/EnemyShip/src/main/java/module-info.java
@@ -1,5 +1,14 @@
+import dk.sdu.mmmi.cbse.common.bullet.BulletSPI;
+import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
+import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
+
 module EnemyShip {
     requires Common;
     requires CommonEnemy;
     requires CommonBullet;
+
+    provides IGamePluginService with dk.sdu.mmmi.cbse.enemyshipsystem.EnemyShipPlugin;
+    provides IEntityProcessingService with dk.sdu.mmmi.cbse.enemyshipsystem.EnemyShipMovementSystem;
+
+    uses dk.sdu.mmmi.cbse.common.bullet.BulletSPI;
 }
