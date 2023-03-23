1914584ae49e662436b07962d46a39e985f7ee48
Cecilie Fredsgaard <ceciliefredsgaard96@gmail.com>
2023-03-23 14:49:14 +0100

    Update module-info for Common

--- AsteroidsServiceLoader/Common/src/main/java/module-info.java
+++ AsteroidsServiceLoader/Common/src/main/java/module-info.java
@@ -1,9 +1,7 @@
 module Common {
     //Requires nothing, is required by every other module
-
     // Exports
     exports dk.sdu.mmmi.cbse.common.services;
-    exports dk.sdu.mmmi.cbse.common.util;
     exports dk.sdu.mmmi.cbse.common.data.entityparts;
     exports dk.sdu.mmmi.cbse.common.data;
 }
