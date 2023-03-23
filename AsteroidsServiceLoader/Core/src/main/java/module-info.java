d7ab5acd762c76299a9822a7130d2feac379eab2
Cecilie Fredsgaard <ceciliefredsgaard96@gmail.com>
2023-03-23 14:48:02 +0100

    Update module-info for Core Module

--- AsteroidsServiceLoader/Core/src/main/java/module-info.java
+++ AsteroidsServiceLoader/Core/src/main/java/module-info.java
@@ -6,7 +6,6 @@ module Core {
     requires CommonBullet;
     requires java.desktop;
     requires com.badlogic.gdx;
-    requires gdx.backend.lwjgl; // This fixed the issue with clean install not finding libgdx package
 
     uses dk.sdu.mmmi.cbse.common.services.IGamePluginService;
     uses dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
